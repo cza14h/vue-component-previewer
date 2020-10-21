5f0c0b98fd371c3a9a96da9bbfba792771d0e467
cza14h <chen_ziao@126.com>
2020-10-21 22:55:37 +0800

    Add project initialized

---
+++ src/views/editor/codeEditor.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/cobalt.css'
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/mode/css/css.js'
+import 'codemirror/mode/xml/xml.js'
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/mode/markdown/markdown.js'
+import 'codemirror/mode/python/python.js'
+import 'codemirror/mode/r/r.js'
+import 'codemirror/mode/shell/shell.js'
+import 'codemirror/mode/sql/sql.js'
+import 'codemirror/mode/swift/swift.js'
+import 'codemirror/mode/vue/vue.js'
+export default {
+  name: 'CodeEditor'
+}
+</script>
+
+<style>
+</style>
